dc82c1846766e4a6190b2b3380feaf656844849c
tcooper <tcooper@localhost>
2016-04-06 17:02:22 -0400

    coded both classes

--- TestTwoTSC/src/edu/saintjoe/cs/tcooper/testtwotsc/TestTylerSpecs.java
+++ TestTwoTSC/src/edu/saintjoe/cs/tcooper/testtwotsc/TestTylerSpecs.java
@@ -3,8 +3,13 @@ package edu.saintjoe.cs.tcooper.testtwotsc;
 public class TestTylerSpecs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TylerSpecs Tyler1 = new TylerSpecs();//create new object
+		Tyler1.name = "Jimmy";//set objects name is Jimmy
+		Tyler1.setName("Tyler");//change Jimmy to Tyler
+		Tyler1.weight = 155;//set Tyler's weight to 155
+		Tyler1.setWeight(165);//Tyler went to the gym and gained muscle cause he's a boss
+		
+		System.out.println(Tyler1.name + "'s weight is " + Tyler1.weight + " pounds!");
 	}
 
 }
--- TestTwoTSC/src/edu/saintjoe/cs/tcooper/testtwotsc/TylerSpecs.java
+++ TestTwoTSC/src/edu/saintjoe/cs/tcooper/testtwotsc/TylerSpecs.java
@@ -1,5 +1,14 @@
 package edu.saintjoe.cs.tcooper.testtwotsc;
 
 public class TylerSpecs {
-
+	public String name;
+	public int weight;
+	
+	/*-------------------------------------------------------------------*/
+	public void setName (String newName) {
+		name = newName;//allow you to change name in driver class
+	}
+	public void setWeight (int newWeight) {
+		weight = newWeight;//allow you to change weight in driver class
+	}
 }
